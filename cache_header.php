8128a054bfa9fe7ac44b3ab06e36631da67cb689
Hill <hill@kimo.com>
2016-08-20 08:52:45 +0000

    fixed when init set to zero, could return no-cache header

--- cache_header.php
+++ cache_header.php
@@ -10,7 +10,7 @@ class cache_header extends \PMVC\PlugIn
     function init()
     {
         $this->setDefaultAlias(new CacheHeaderHelper());
-        if (!empty($this[0])) {
+        if (!empty($this[0]) || strlen($this[0])) {
             \PMVC\callPlugin(
                 \PMVC\getOption(_ROUTER),
                 'processHeader',
